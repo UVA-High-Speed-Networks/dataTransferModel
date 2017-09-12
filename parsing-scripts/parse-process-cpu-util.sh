f4990c9d28a338d66a64a83ba3c7a5726b91045a
Sourav Maji <sm8kk@virginia.edu>
2017-09-12 13:23:39 -0400

    Add files via upload

--- parsing-scripts/parse-process-cpu-util.sh
+++ parsing-scripts/parse-process-cpu-util.sh
@@ -1,4 +1,5 @@
 fileName=$1
-cat $fileName | grep "iperf3" | tr -s " " | awk 'BEGIN {FS = " "}; {print $9}' > processCpuUtil.txt
-avgProcCpu=`python avgMetric.py processCpuUtil.txt`
+processName=$2
+cat $fileName | grep $processName | tr -s " " | awk 'BEGIN {FS = " "}; {print $9}' > processCpuUtil.txt
+avgProcCpu=`python avgMetricNonZero.py processCpuUtil.txt`
 echo $avgProcCpu
